9b0f45bea5213bd039aa4739c90503df779c1ea2
Zac Heisey <zheisey@gmail.com>
2019-06-20 16:36:24 -0400

    set up test api exercise for FEWD class

---
+++ main.js
@@ -0,0 +1,50 @@
+// Get app container
+var app = document.querySelector('#app');
+
+// Load application
+var loadApp = function() {
+
+  // Set up XHR request
+  var xhr = new XMLHttpRequest();
+  // Setup our listener to process request state changes
+  xhr.onreadystatechange = function() {
+      // Only run if the request is complete
+      if (xhr.readyState !== 4) return;
+      // Process our return data
+      if (xhr.status >= 200 && xhr.status < 300) {
+          // This will run when the request is successful
+          renderAppMarkup(JSON.parse(xhr.responseText));
+      } else {
+          // This will run when it's not
+          app.innerHTML = 'The request failed! Please try again later';
+          console.log(xhr);
+      }
+  };
+  // Create and send a GET request
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+  xhr.send();
+
+  // Render markup with returned API data
+  function renderAppMarkup(data) {
+
+    // Create 3 random numbers and store them in an array
+    var randomArray = [];
+    for (var i = 0; i < 3; i++) {
+      var randomIndex = Math.floor(Math.random() * 100);
+      randomArray.push(randomIndex);
+    }
+    // Set up HTML string
+    var html = '';
+    // Add markup for each of the 3 randomly selected posts
+    randomArray.forEach(function(index) {
+      html += '<h1>' + data[index].title + '</h1>';
+    });
+    // Inject HTML into the DOM
+    app.innerHTML = html;
+
+  }
+
+} // end of loadApp function
+
+// Run loadApp function on page load
+window.addEventListener('load', loadApp, false);
