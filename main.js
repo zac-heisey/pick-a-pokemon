c33b82d9979801cd14e4bf0b4292e7707934da0e
Zac Heisey <zheisey@gmail.com>
2019-06-27 13:05:17 -0400

    set up url params function to get query strings

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 // Get app container
 var app = document.querySelector('#app');
 
+/**
+ * Get the URL parameters
+ * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+ */
+var getParams = function (url) {
+  var params = {};
+  var parser = document.createElement('a');
+  url = url || window.location.href;
+  parser.href = url;
+  var query = parser.search.substring(1);
+  var vars = query.split('&');
+  for (var i=0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      params[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return params;
+};
+
+// Cache URL parameters
+var params = getParams();
+
 // Create function to load app
 var loadApp = function() {
 
@@ -27,28 +48,36 @@ var loadApp = function() {
   // Render markup with returned API data
   function renderAppMarkup(data) {
 
-    // Create 3 random numbers and store them in an array
-    randomArray = [];
-    for (var i = 0; i < 3; i++) {
-      var randomIndex = Math.floor(Math.random() * 100);
-      randomArray.push(randomIndex);
-    }
     // Set up HTML string
     var html = '';
-    // Add markup for each of the 3 randomly selected Pokemon
-    randomArray.forEach(function(index) {
-      html +=
-        '<div class="pokemon-cards">' +
-          '<h2>' + data.results[index].name.toUpperCase() + '</h2>' +
-          '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (index + 1) + '.png">' +
-        '</div>';
-    });
+    // Check for URL parameters
+    if (params.name === undefined) {
+      // Create 3 random numbers and store them in an array
+      randomArray = [];
+      for (var i = 0; i < 3; i++) {
+        var randomIndex = Math.floor(Math.random() * 100);
+        randomArray.push(randomIndex);
+      }
+      // Add markup for each of the 3 randomly selected Pokemon
+      randomArray.forEach(function(index) {
+        html +=
+          '<div class="pokemon-cards">' +
+            '<a href="?name=' + data.results[index].name + '&id=' + (index + 1) + '">' +
+              '<h2>' + data.results[index].name.toUpperCase() + '</h2>' +
+              '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + (index + 1) + '.png">' +
+            '</a>' +
+          '</div>';
+      });
+    } else {
+      // Render individual Pokemon card info based on URL param data (name, id)
+    }
+
     // Inject HTML into the DOM
     app.innerHTML = html;
 
-  }
+  } // End of renderAppMarkup function
 
-} // end of loadApp function
+} // End of loadApp function
 
 // Run loadApp function on page load
 loadApp();
